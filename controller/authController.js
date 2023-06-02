7b72ba336cdeb91d72d9987e7c846db62fc71b61
letuyen <letuyenkhtn212@gmail.com>
2023-06-02 18:10:28 +0700

    end

--- controller/authController.js
+++ controller/authController.js
@@ -16,7 +16,7 @@ const createSendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        // httpOnly: true,
         // secure: true
     };
     // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
