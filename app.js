8cb7e990519656597f93ded1739d538cbaf812d1
Mặt trăng khuyết <121482518+Tuyenleuit212@users.noreply.github.com>
2023-06-03 11:41:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin : "https://shop-myway-frontend.vercel.app"
+        origin : "https://myway-shop-app.onrender.com"
     }
 ))
 app.use(express.static('public/image'));
