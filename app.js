7cf511bd3fa58b976b8d3bb8c9c62335e4bff5d2
nguyen ngoc khanh <84921391+hellokhanhne@users.noreply.github.com>
2023-06-04 10:52:06 +0700

    cm

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const mongoSanitize = require('express-mongo-sanitize');
-const productRouter = require('./routes/productRoutes')
-const userRouter = require('./routes/userRoutes')
-const cartRouter = require('./routes/cartRoutes')
-const bookingRouter = require('./routes/bookingRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const chatRouter = require('./routes/chatRoutes')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const productRouter = require("./routes/productRoutes");
+const userRouter = require("./routes/userRoutes");
+const cartRouter = require("./routes/cartRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const chatRouter = require("./routes/chatRoutes");
+const app = express();
 
-app.use(cors(
-    {
-        origin : "https://myway-shop-app.onrender.com"
-    }
-))
-app.use(express.static('public/image'));
+app.use(
+  cors({
+    origin: "https://web-fux0.onrender.com",
+  })
+);
+app.use(express.static("public/image"));
 
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(mongoSanitize());
-app.get('/' , (req,res) => {
-    res.status(200).json({
-        status : 'success',
-        message : "hello , build success"
-    })
-})
-app.use('/myway/api/products', productRouter)
-app.use('/myway/api/users' , userRouter)
 
-app.use('/myway/api/carts' , cartRouter)
-app.use('/myway/api/bookings' , bookingRouter)
-app.use('/myway/api/reviews' , reviewRouter)
-app.use('/myway/api/chats' , chatRouter)
-module.exports = app
+app.use("/myway/api/products", productRouter);
+app.use("/myway/api/users", userRouter);
+
+app.use("/myway/api/carts", cartRouter);
+app.use("/myway/api/bookings", bookingRouter);
+app.use("/myway/api/reviews", reviewRouter);
+app.use("/myway/api/chats", chatRouter);
+
+app.use(express.static("frontend/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
+module.exports = app;
